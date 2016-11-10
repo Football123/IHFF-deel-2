37dce7935277abb6288c2254aac052165dd9e1dc
Martin <junocaen@hotmail.com>
2016-11-10 13:34:14 +0100

    project in .net mvc 4.5 aangemaakt

---
+++ ifhhA7/ifhhA7/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ifhhA7.Startup))]
+namespace ifhhA7
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
