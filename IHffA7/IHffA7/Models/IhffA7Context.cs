4ec3f18ac1fd65e54942976ea9abee44576ed218
Martin <junocaen@hotmail.com>
2016-11-18 08:27:11 +0100

    Begin db context

---
+++ IHffA7/IHffA7/Models/IhffA7Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace IHffA7.Models
+{
+    class IhffA7Context : DbContext
+    {
+        public IhffA7Context() : base("iHFF1617S_A7")
+        {
+            
+        }
+        //public DbSet<>
+    }
+}
