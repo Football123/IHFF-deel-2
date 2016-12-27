f60154c3eb27207cbd931fb91a749ddcd8743bd5
Martin <junocaen@hotmail.com>
2016-12-27 15:42:04 +0100

    start activitiesmanager

---
+++ IHffA7/IHffA7/Models/repositories/ActivitiesManagerRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHffA7.Models.repositories
+{
+    class ActivitiesManagerRepository
+    {
+        WhatsUp1617S_martinstinsGenerated ctx = new WhatsUp1617S_martinstinsGenerated(); //test offline db
+    }
+}
