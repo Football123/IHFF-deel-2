677ee157a6c13ff9610788ddcfecc89ad35c82ad
MartijnBeljaards <MartijnBeljaards@MARTIJN>
2017-01-10 17:01:32 +0100

    Update 10 januari 2017 - 17:00 u (Home + Restaurant pagina's)

--- IHffA7/IHffA7/Models/repositories/DinnerRepository.cs
+++ IHffA7/IHffA7/Models/repositories/DinnerRepository.cs
@@ -9,49 +9,33 @@ namespace IHffA7.Models.repositories
     {
         //private IhffA7Context ctx = new IhffA7Context();
         private WhatsUp1617S_martinstinsGenerated ctx = new WhatsUp1617S_martinstinsGenerated();
+
         public IEnumerable<RestaurantOverviewModel> GetAllRestaurants()
         {
-            IList<RestaurantOverviewModel> restaurantItems = new List<RestaurantOverviewModel>() ;
-            foreach( var res in ctx.Restaurants)
-            {
-                RestaurantOverviewModel r = new RestaurantOverviewModel();
-                r.DescriptionNL = res.descriptionNL;
-                r.DinnerEnd = r.DinnerEnd;
-                r.DinnerStart = res.dinnerStart;
-                r.Id = res.id;
-                r.LunchStart = res.lunchStart;
-                r.LunchEnd = res.lunchEnd;
-                r.Name = res.Locations.name;
-                r.RestaurantLogo = res.restaurantLogo;
-                var restLocation = res.Locations;
-                r.Street = restLocation.street;
-                r.StreetNumber = restLocation.streetNumber;
-                r.Town = restLocation.town;
-                r.ZipCode = restLocation.zipCode;
-                restaurantItems.Add(r);
-            }
-            /*(
-                from r in ctx.Restaurant
-                join a in ctx.Activity
-                on r.Id equals a.Id
-                join l in ctx.Location
-                on a.Id equals l.Id
+            IEnumerable<RestaurantOverviewModel> restaurantItems =
+            (
+                from r in ctx.Restaurants
+                join l in ctx.Locations
+                    on r.locationId equals l.id
                 select new RestaurantOverviewModel
                 {
-                    Id = r.Id,
-                    Name = l.Name,
-                    DescriptionNL = r.DescriptionNL,
-                    Street = l.Street,
-                    StreetNumber = l.StreetNumber,
-                    ZipCode = l.ZipCode,
-                    Town = l.Town,
-                    LunchStart = r.LunchStart,
-                    LunchEnd = r.LunchEnd,
-                    DinnerStart = r.DinnerStart,
-                    DinnerEnd = r.DinnerEnd,
-                    RestaurantLogo = r.RestaurantLogo
+                    Id = r.id,
+                    Name = l.name,
+                    DescriptionNL = r.descriptionNL,
+                    Street = l.street,
+                    StreetNumber = l.streetNumber,
+                    ZipCode = l.zipCode,
+                    Town = l.town,
+                    LunchStart = r.lunchStart,
+                    LunchEnd = r.lunchEnd,
+                    DinnerStart = r.dinnerStart,
+                    DinnerEnd = r.dinnerEnd,
+                    RestaurantLogo = r.restaurantLogo,
+                    Datum = null,
+                    Tijd = null,
+                    AantalPersonen = null
                 }
-            )*/;
+            );
 
             return restaurantItems;
         }
---
+++ IHffA7/IHffA7/Models/repositories/HomeRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Web;
+
+namespace IHffA7.Models.repositories
+{
+    public class HomeRepository : IHomeRepository
+    {
+        private WhatsUp1617S_martinstinsGenerated ctx = new WhatsUp1617S_martinstinsGenerated();
+
+        public IEnumerable<FilmsTodayModel> GetTodaysMovies()
+        {
+            var today = DateTime.Today.Date;
+
+            var q = ctx.Activities.Select(t => t.startTime.Date);
+
+            IEnumerable<FilmsTodayModel> filmTodayItems =
+            (
+                from f in ctx.Films
+                join fs in ctx.Filmscreenings
+                    on f.id equals fs.filmId
+                join r in ctx.Rooms
+                    on fs.roomId equals r.id
+                join a in ctx.Activities
+                    on fs.activityId equals a.id
+                    where System.Data.Entity.DbFunctions.TruncateTime(a.startTime) == today
+                join l in ctx.Locations
+                    on r.locationId equals l.id
+                select new FilmsTodayModel
+                { 
+                    filmLogo = f.filmLogo,
+                    beginTime = a.startTime, 
+                    endTime = a.endTime,
+                    locationName = l.name,
+                    roomName = r.name,
+                    availableSeats = fs.availableSeats,
+                    title = f.title,
+                    price = a.price
+                }
+            );
+
+            return filmTodayItems;
+        }
+    }
+}
